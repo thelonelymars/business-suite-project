5876598bd5dc30ac77f46b7e9dcc402abdf94e40
mshar <email@mail.com>
2023-08-20 04:35:27 +0300

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   .vscode/launch.json
    	new file:   Calculator.html
    	new file:   calendar.html
    	new file:   img/Free-Download-Abstract-Backgrounds-HD-1105343321.jpeg
    	new file:   img/abstract_nature.jpg
    	new file:   img/abstract_nature_mediom.jpg
    	new file:   img/abstract_nature_small.jpg
    	new file:   index.css
    	new file:   index.html
    	new file:   index.js
    	new file:   templetes/cacl_4.png
    	new file:   templetes/calc_1.png
    	new file:   templetes/calc_2.png
    	new file:   templetes/calc_3.jpeg
    	new file:   templetes/toDolist_templete.jpeg
    	new file:   toDolist.html

---
+++ index.js
@@ -0,0 +1,70 @@
+const taskInput = document.getElementById('input_bt');
+const addTaskButton = document.getElementById('button_black');
+const taskList = document.getElementById('taskList');
+const number_btn=document.querySelectorAll(".numbers_Btn");
+const TEXTFIELD=document.getElementById('input_field1');
+let currentInput="";
+
+
+addTaskButton.addEventListener('click', addTask);
+function addTask() {
+  const taskText = taskInput.value.trim();
+  if (taskText === '') return;
+
+  const taskId = generateUniqueId();
+  const taskItem = createTaskItem(taskId, taskText);
+
+  taskList.appendChild(taskItem);
+  taskInput.value = '';
+}
+
+function createTaskItem(id, text) {
+  const taskItem = document.createElement('li');
+  taskItem.innerHTML = `
+    <input type="checkbox" id="${id}">
+    <label for="${id}">${text}</label>
+    <button data-id="${id}" class="btn btn-outline-dark btn-sm removeTaskButton">Remove</button>
+  `;
+
+  const checkbox = taskItem.querySelector('input[type="checkbox"]');
+  checkbox.addEventListener('change', toggleTask);
+
+  const removeButton = taskItem.querySelector('.removeTaskButton');
+  removeButton.addEventListener('click', removeTask);
+
+  return taskItem;
+}
+
+function toggleTask(event) {
+  const taskId = event.target.id;
+  const label = taskList.querySelector(`label[for="${taskId}"]`);
+  label.classList.toggle('completed');
+}
+
+function removeTask(event) {
+  const taskId = event.target.dataset.id;
+  const taskItem = document.getElementById(taskId);
+  if (taskItem) {
+    taskList.removeChild(taskItem.parentNode);
+  }
+}
+
+function generateUniqueId() {
+  return Date.now().toString(36) + Math.random().toString(36).substr(2, 5);
+}
+
+document.querySelector("button.reset").addEventListener("click",function(){
+    let list = document.querySelector("ol")
+    while (list.hasChildNodes()) {
+        list.removeChild(list.firstChild);
+      }
+})
+function number_text(number) {
+  currentInput += number;
+  TEXTFIELD.value=currentInput
+}
+function clear_numbers(){
+  currentInput=""
+  TEXTFIELD.value=""
+}
+
